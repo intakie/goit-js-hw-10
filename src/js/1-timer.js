357bc1bae49e0c7396b584ee6fa2e61703dceab7
intakie <intakiea@gmail.com>
2024-03-27 14:54:36 +0200

    upd timer js css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const input = document.getElementById('datetime-picker');
 const startBtn = document.querySelector('button');
 
@@ -15,8 +18,15 @@ const options = {
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
     const now = new Date();
+
     if (userSelectedDate < now) {
-      window.alert('Please choose a date in the future');
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        backgroundColor: 'red',
+        messageColor: 'white',
+        messageSize: '16',
+        position: 'topRight',
+      });
       startBtn.disabled = true;
     } else {
       startBtn.disabled = false;
@@ -26,4 +36,50 @@ const options = {
 
 startBtn.disabled = true;
 
-flatpickr(input, options);
+const fp = flatpickr(input, options);
+
+startBtn.addEventListener('click', () => {
+  input.disabled = true;
+  startBtn.disabled = true;
+
+  const countdownInterval = setInterval(() => {
+    const now = new Date();
+    const difference = userSelectedDate.getTime() - now.getTime();
+
+    if (difference <= 0) {
+      clearInterval(countdownInterval);
+      updateTimer(0, 0, 0, 0);
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(difference);
+    updateTimer(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function updateTimer(days, hours, minutes, seconds) {
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent =
+    addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent =
+    addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
